6abfc88d30c4ac8277fe6a4ad1a371761a42e139
WafaMansouri <62021981+WafaMansouri@users.noreply.github.com>
2020-12-29 13:42:41 +0100

    Update add_photo.js

--- routes/add_photo.js
+++ routes/add_photo.js
@@ -8,7 +8,7 @@ const multer = require("multer");
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, `http://localhost:4000/uploads/profile_photos`);
+    cb(null, `C:\Users\donia-mansouri\Desktop\GoMyCode\Project\project dev\uploads\profile_photos`);
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
