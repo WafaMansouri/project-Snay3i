02faaf91ef711c32cc3811a33377dde7caaa0f4e
WafaMansouri <62021981+WafaMansouri@users.noreply.github.com>
2020-12-30 14:06:41 +0100

    Update store.js

--- front/project/src/store/store.js
+++ front/project/src/store/store.js
@@ -3,9 +3,9 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(thunk),
+    process.env.NODE_ENV !== "production" &&
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 export default store;
